31439c5db13541c23203a1dcd2cc7790e6f352de
Spec-Tr <142240560+Spec-Tr@users.noreply.github.com>
2024-01-25 20:31:08 -0500

    added routing to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,28 @@ import ClubPage from "./pages/ClubPage";
 import DropdownForm from "./components/DropdownForm";
 
 function App() {
+  const [token, setToken] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [userId, setUserId] = useState(0);
+  const handleLogin = loginObj=>{
+    API.login(loginObj).then(loginData=>{
+      setToken(loginData.token);
+      setIsLoggedIn(true);
+      setUserId(loginData.user.id)
+    }).catch(err=>{
+      console.log('err', err)
+    })
+  }
+  const handleSignup = signupObj=>{
+    API.signup(signupObj).then(loginData=>{
+      setToken(loginData.token);
+      setIsLoggedIn(true);
+      setUserId(loginData.user.id)
+    }).catch(err=>{
+      console.log('err', err)
+    })
+  }
+
   return (
     // <div>
     //   <h1> hello</h1>
@@ -17,7 +39,12 @@ function App() {
 {/* <NavBar/> */}
   <Routes>
     <Route path="/" element={<DropdownForm/>}/>
+    <Route path="/account" element={<Account/>}/>
     <Route path="/clubs" element={<ClubPage/>}/>
+    <Route path="/competitions" element={<CompetitionPageLogin/>}/>
+    <Route path="/login" element={<AuthForm type="login" handleSubmit={handleLogin}/>}/>
+    <Route path="/signup" element={<AuthForm type="signup" handleSubmit={handleSignup}/>}/>
+    <Route path="/players" element={<PlayerPage/>}/>
   </Routes>
 </Router>
 </>
