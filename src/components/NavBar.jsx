b7686fa1f5c1f4ac29a15b661d65a6f554b0fc34
Spec-Tr <142240560+Spec-Tr@users.noreply.github.com>
2024-01-30 18:44:46 -0500

    more logout pieces

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function NavBar(props) {
+export default function NavBar({isLoggedIn, handleLogout}) {
     
     return (
         <div className="NavBar">
-           <Link to="/">Home</Link>
-           <Link to="/login">Login</Link>
-           <Link to="/logout">Logout</Link>
-           <Link to="/signup">Signup</Link>
+            <Link to="/">Home</Link>
+      <Link to="/login">Login</Link>
+      {isLoggedIn ? (
+        <Link to="/" onClick={handleLogout}>Logout</Link>
+      ) : (
+        <Link to="/signup">Signup</Link>
+      )}
         </div>
     )
 }
