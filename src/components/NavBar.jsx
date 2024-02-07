40334868512b28bdf54cbd3e50da81aaafe591e6
Rahul Basu <142247733+r-basu@users.noreply.github.com>
2024-02-06 19:23:33 -0500

    Add protected routes and redirections

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,26 @@
 import { Link } from "react-router-dom";
 
-
-
 export default function NavBar({ isLoggedIn, handleLogout, logo }) {
   return (
     <div className="flex justify-end bg-gray-400 p-4">
       <img src={logo} alt="Logo" className="h-20" />
       <div className="NavBar ml-auto flex items-center">
-        <Link to="/" className="text-lg mx-4">Home</Link>
-        <Link to="/login" className="text-lg mx-4">Login</Link>
-        {isLoggedIn ? (
+        <Link to="/" className="text-lg mx-4">
+          Home
+        </Link>
+        {!isLoggedIn && (
+          <>
+            <Link to="/login" className="text-lg mx-4">
+              Login
+            </Link>
+            <Link to="/signup" className="text-lg mx-4">
+              Signup
+            </Link>
+          </>
+        )}
+        {isLoggedIn && (
           <Link to="/" onClick={handleLogout} className="text-lg mx-4">
             Logout
-          </Link>
-        ) : (
-          <Link to="/signup" className="text-lg mx-4">
-            Signup
           </Link>
         )}
       </div>
